f21929bbbd40871624ec20ee9d381c85a72da94b
Alvaro De la Rica <alvaro.delarica.alvarez@gmail.com>
2022-06-26 17:22:39 +0200

    retrieve data test

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import prisma from 'lib/prisma'
+
+export default async function handler(req, res) {
   if (req.method !== 'GET') {
     res.status(405).json({
       message: 'Method Not Allowed'
@@ -6,5 +8,6 @@ export default function handler(req, res) {
     return
   }
 
-  res.status(200).json({ test: 'TRIPS!' })
+  const trips = await prisma.trip.findMany()
+  res.status(200).json(trips)
 }
